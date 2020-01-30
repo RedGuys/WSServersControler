88edfe908135ea42fc1215a9105d1e2b411e6e40
Ilya <admin@redguy.ru>
2020-01-30 12:13:13 +0300

    Added start and onConnection to the test module

--- modules/echo/index.js
+++ modules/echo/index.js
@@ -1,3 +1,12 @@
+exports.start = () => {
+    console.log("I'm started!");
+};
+
+exports.onConnection = (ws) => {
+    console.log("Connected new client!");
+};
+
 exports.onMessage = (message,ws) => {
     ws.send(message);
+    console.log("I getted and recevived message: "+message);
 };
